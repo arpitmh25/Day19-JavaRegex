58ac0afce7e4240448eb2e33af2029101e818a87
Arpit <arpitmh23@gmail.com>
2022-01-09 19:16:13 +0530

    Adding uc9

--- src/main/java/com/bridgelabz/regex/day19/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/day19/UserRegistration.java
@@ -5,29 +5,61 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String FIRST_NAME = "^[A-Z][a-z]{2,}$";
-    private static final String LAST_NAME= "^[A-Z][a-z]{2,}$";
-    private static final String EMAIL= "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+[.a-z]+$";
-    private static final String PHONE_NUMBER = "^[0-9]{2}[\\s][0-9]{10}$";
+    private static final String LAST_NAME = "^[A-Z][a-z]{2,}$";
+    private static final String EMAIL = "^[a-zA-Z0-9_-]+(?:\\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$";
+    private static final String PHONE_NUMBER = "^[0-9]{2}[0-9]{10}";
     private static final String PASSWORD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9].{8,}$";
+    static boolean isFirstNameValid, isLastNameValid, isMailValid, isNumberValid, isPasswordValid;
 
-
-    public void register(User userDetail) {
-        boolean isFirstNameValid = validateUserDetails(FIRST_NAME, userDetail.getFirstName());
-        boolean isLastNameValid = validateUserDetails(LAST_NAME, userDetail.getLastName());
-        boolean isEmailValid = validateUserDetails(EMAIL, userDetail.getEmail());
-        boolean isPhoneNumberValid = validateUserDetails(PHONE_NUMBER, userDetail.getPhoneNumber());
-        boolean isPasswordValid = validateUserDetails(PASSWORD, userDetail.getPassword());
-
-        if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
-            System.out.println("Details entered are valid");
-        } else {
-            System.out.println("Details entered are invalid");
+    public boolean validateFirstName(String firstName) {
+        isFirstNameValid = Pattern.matches(FIRST_NAME, firstName);
+        if (!isFirstNameValid) {
+            System.out.println("First Name is invalid...Please enter proper first name");
+            System.exit(0);
         }
+        return Pattern.matches(FIRST_NAME, firstName);
     }
 
-    private boolean validateUserDetails(String regex, String dataToBeValidated) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(dataToBeValidated);
-        return matcher.matches();
+    public boolean validateLastName(String lastName) {
+        isLastNameValid = Pattern.matches(LAST_NAME, lastName);
+        if (!isLastNameValid) {
+            System.out.println("Last Name is invalid...Please enter proper last name");
+            System.exit(0);
+        }
+        return Pattern.matches(LAST_NAME, lastName);
+    }
+
+    public boolean validateEmail(String mail) {
+        isMailValid = Pattern.matches(EMAIL, mail);
+        if (!isMailValid) {
+            System.out.println("E-mail is invalid...Please enter e-mail");
+            System.exit(0);
+        }
+        return Pattern.matches(EMAIL, mail);
+    }
+
+    public boolean validatePhoneNumber(String phoneNumber) {
+        isNumberValid = Pattern.matches(PHONE_NUMBER, phoneNumber);
+        if (!isNumberValid) {
+            System.out.println("Phone Number is invalid...Please enter proper phone-number");
+            System.exit(0);
+        }
+        return Pattern.matches(PHONE_NUMBER, phoneNumber);
+    }
+
+    public boolean validatePassword(String password) {
+        isPasswordValid = Pattern.matches(PASSWORD, password);
+        if (!isPasswordValid) {
+            System.out.println("Password is invalid...Please enter proper password");
+            System.exit(0);
+        }
+        return Pattern.matches(PASSWORD, password);
+    }
+
+    public void register() {
+        if (isFirstNameValid && isLastNameValid && isMailValid && isNumberValid & isPasswordValid) {
+            System.out.println("You are registered!!!");
+            System.out.println("Welcome");
+        }
     }
 }
